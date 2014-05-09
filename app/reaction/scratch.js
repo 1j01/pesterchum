55f936122753afa3dbe660a796c7a2e43461e0b4
Isaiah Odhner <isaiahodhner@gmail.com>
2014-05-09 15:22:19 -0400

    fix DSRGIFG and clean it up

--- app/reaction/scratch.js
+++ app/reaction/scratch.js
@@ -1,31 +1,24 @@
 
 (function(){
-	//var GIF = require("gif");
 	var fontDummy = new Image();
 	var loaded = false;
 	fontDummy.onerror = function(){
-		console.log("font loaded, probably");
 		loaded = true;
 	};
 	fontDummy.src = "../FontStuck-Extended.ttf";
 
-	$DSRG = function(text){
+	$DocScratchReaction = function(text){
 		text = text.toUpperCase();
 		var t = 0;
 
-		var $canvas = $("<canvas>");
+		var $canvas = $(document.createElement("canvas"));
 		var canvas = $canvas[0];
 		var ctx = canvas.getContext("2d");
 
 		var interval = 50;
 		var recording = false;
 		var recordingLength = 0;
-		var gif = new GIF({
-			workers: 2,
-			quality: 10
-		});
-
-		var ff = 'fontstuck';
+		var ff = "fontstuck";
 		var px = "px ";
 		var fs = 50;
 		ctx.font = fs+px+ff;
@@ -34,11 +27,11 @@
 		canvas.height = fs*1;
 		var textY = canvas.height * 4/4;
 
-		var lightning = function(life,x,y,xd,yd,xtend){
-			if(life--<0)return;
+		var lightning = function(life, x, y, xd, yd, xtend){
+			if(life--<0) return;
 
-			const r = 4;
-			const r2 = 6;
+			var r = 4;
+			var r2 = 6;
 
 			if(y<textY-fs){
 				yd+=2;
@@ -47,27 +40,28 @@
 				yd-=2;
 			}
 
-			var nx, ny;
-			nx = x + xd*Math.random()*5 + xtend*Math.random()*2;
-			ny = y + yd*Math.random()*5;
+			var nx = x + xd*Math.random()*5 + xtend*Math.random()*2;
+			var ny = y + yd*Math.random()*5;
 			ctx.lineWidth = 2;
 			ctx.beginPath();
-			ctx.moveTo(x,y);
-			ctx.lineTo(nx,ny);
+			ctx.moveTo(x, y);
+			ctx.lineTo(nx, ny);
 			ctx.stroke();
 			lightning(
 				life*0.9,
-				nx,ny,
+				nx, ny,
 				(xd+Math.random()*r-r/2)/2,
 				(yd+Math.random()*r-r/2)/2,
-				xtend);
+				xtend
+			);
 			if(Math.random()<0.1 && life>10){
 				lightning(
 					life*0.49,
-					nx,ny,
+					nx, ny,
 					(xd+Math.random()*r2-r2/2)/2,
 					(yd+Math.random()*r2-r2/2)/2,
-					xtend);
+					xtend
+				);
 			}
 		};
 
@@ -75,33 +69,32 @@
 		var animate = function(){
 			ctx.font = fs+px+ff;
 			ctx.textBaseline = "bottom";
-			//if(Math.random()<0.9)
-			ctx.clearRect(0,0,canvas.width,canvas.height);
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 			//raylike shadowy stuff
 			if(t>=4 && t<=6 || t==19 || t==22){
 				var start = 0, end = 10;
 				ctx.fillStyle = "rgba(0,0,0,0.1)";
-				if(t==5)ctx.fillStyle = "rgba(0,0,0,0.3)";
-				if(t==6)ctx.fillStyle = "rgba(0,0,0,0.07)";
-				if(t==6)start = 7, end = 17;
-				for(var i=start;i<end;i++){
-					ctx.fillText(text,i,textY);
+				if(t==5) ctx.fillStyle = "rgba(0,0,0,0.3)";
+				if(t==6) ctx.fillStyle = "rgba(0,0,0,0.07)";
+				if(t==6) start = 7, end = 17;
+				for(var i=start; i<end; i++){
+					ctx.fillText(text, i, textY);
 				}
 			}
 			if(t==14){
 				var l = 10;
 				ctx.fillStyle = "rgba(0,0,0,0.4)";
-				ctx.fillText(text,0,textY-2);
+				ctx.fillText(text, 0, textY-2);
 				ctx.fillStyle = "rgba(255,255,255,0.4)";
-				ctx.fillText(text,0,textY-1);
+				ctx.fillText(text, 0, textY-1);
 			}
 			if(t==15 || t==19){
 				var l = 10;
 				ctx.fillStyle = "rgba(0,0,0,0.1)";
-				if(t==14)l=17;
-				for(var i=0;i<l;i++){
-					ctx.fillText(text,0,textY-i);
+				if(t==14) l=17;
+				for(var i=0; i<l; i++){
+					ctx.fillText(text, 0, textY-i);
 				}
 			}
 
@@ -128,23 +121,22 @@
 			if(t==21){
 				ctx.fillStyle = "#AAA";
 			}
-			ctx.fillText(text,0,textY);
+			ctx.fillText(text, 0, textY);
 
 			//secondary black text that sort of zooms a bit
 			if(t>=16 && t<19){
 				ctx.font = (fs+2*(19-t))+px+ff;
 				ctx.fillStyle = "#000";
-				ctx.fillText(text,0,textY);
+				ctx.fillText(text, 0, textY);
 			}
 
 			//draw... lightning!
 			ctx.strokeStyle = "yellow";
-			//if((t%8)==0){
 			if(Math.random()<0.3){
 				lx = Math.random()*textWidth*0.8;
 			}
 			if((t%8)<4){
-				lightning(100,lx,textY-Math.random()*fs,0,Math.random()*2-1,Math.random()*4-2);
+				lightning(100, lx, textY-Math.random()*fs, 0, Math.random()*2-1, Math.random()*4-2);
 			}
 
 			t=(t+1)%23;
@@ -154,22 +146,14 @@
 			}
 			if(recording){
 				if(++recordingLength < 100){
-					gif.addFrame(canvas, {delay:interval});
-				}else{
-					gif.render();
+					$canvas.trigger("frame", [canvas, interval]);
+				}else if(recordingLength == 100){
+					$canvas.trigger("animation-complete");
 				}
-				
 			}
-			//requestAnimationFrame(animate);
 		};
-		//animate();
-		setInterval(animate,interval);
+		setInterval(animate, interval);
 
-		gif.on('finished', function(blob) {
-			$canvas.trigger("gif",$("<img>").attr("src",URL.createObjectURL(blob)));
-		});
-
-		return $canvas;//for now
+		return $canvas;
 	};
-
 })();
